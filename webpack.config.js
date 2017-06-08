a7c0c527f7bc087f48bb96bf31d8ca27c189ce68
Jesus Ferrer <Jesus.FerrerEstupinan@availity.com>
2017-06-08 14:37:00 -0400

    Update libraries and webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const webpack = require('webpack');
+const path = require('path');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const WebpackNotifierPlugin = require('webpack-notifier');
 const autoprefixer = require('autoprefixer');
@@ -27,12 +28,12 @@ const config = {
     'docs': './docs/js'
   },
 
-  devtool: 'source-map',
+  devtool: 'cheap-module-eval-source-map',
 
   output: {
     // if path is not set to '/build' => Error invalid argument in MemoryFileSystem.js.  Also,
     // `output.path` needs to be an absolute path or `/` else error
-    path: '/build',
+    path: path.join(__dirname, 'build'),
     // publicPath: '../',
     filename: 'js/[name].js',
     library: 'availity-uikit',
@@ -40,9 +41,9 @@ const config = {
     umdNamedDefine: true
   },
 
-  debug: true,
-  cache: true,
-  watch: true,
+  // debug: true,
+  // cache: true,
+  // watch: true,
 
   stats: {
     colors: true,
@@ -57,25 +58,50 @@ const config = {
   },
 
   module: {
-    loaders: [
+    rules: [
 
       {
         test: /\.js$/,
-        loader: 'babel',
+        use: 'babel-loader',
         exclude: /(bower_components|node_modules)/
       },
       {
         test: /\.css$/,
-        loader: ExtractTextPlugin.extract('style', 'css')
+        use: ExtractTextPlugin.extract({fallback: 'style-loader', use: 'css-loader'})
       },
       {
         test: /\.less$/,
-        loader: ExtractTextPlugin.extract(
-          'style',
-          'css?limit=32768?name=images/[name].[ext]!postcss!less',
-          {
-            publicPath: '../'
-          }
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: [
+            'css-loader?limit=32768?name=images/[name].[ext]',
+            {
+              loader: 'postcss-loader',
+              options: {
+                sourceMap: true,
+                plugins: [
+                  autoprefixer(
+                    {
+                      browsers: [
+                        'last 5 versions',
+                        'Firefox ESR',
+                        'not ie < 9'
+                      ]
+                    }
+                  )
+                ]
+              }
+            },
+            {
+              loader: 'less-loader',
+              options: {
+                strictMath: true,
+                noIeCompat: true
+              }
+            }
+          ],
+          publicPath: '../'
+        }
         )
       },
       {
@@ -85,40 +111,42 @@ const config = {
         //  '../fonts/availity-font.eot'
         //
         test: /\.(ttf|woff|eot|svg).*/,
-        loader: 'file?name=fonts/[name].[ext]'
+        use: 'file-loader?name=fonts/[name].[ext]'
       },
       {
         test: /\.scss$/,
-        loaders: ['style', 'css', 'sass']
+        use: ['style-loader', 'css-loader', 'sass-loader']
       },
       {
         test: /\.(jpe?g|png|gif)$/,
-        loader: 'url?limit=32768?name=images/[name].[ext]'
+        use: 'url-loader?limit=32768?name=images/[name].[ext]'
       }
     ]
   },
 
-  postcss() {
-    return [autoprefixer({browsers: ['last 2 versions', 'ie 9-11']})];
-  },
+  // postcss() {
+  //   return [autoprefixer({browsers: ['last 2 versions', 'ie 9-11']})];
+  // },
 
-  lessLoader: {
-    lessPlugins: [
-      new NpmImportPlugin({
-        prefix: '~'
-      })
-    ]
-  },
+  // lessLoader: {
+  //   lessPlugins: [
+  //     new NpmImportPlugin({
+  //       prefix: '~'
+  //     })
+  //   ]
+  // },
 
   plugins: [
 
-    new webpack.BannerPlugin(banner(), {
+    new webpack.BannerPlugin({
+      banner: banner(),
       exclude: ['vendor']
     }),
 
-    new webpack.IgnorePlugin(/^\.\/locale$/, [/moment$/]),
+    // new webpack.IgnorePlugin(/^\.\/locale$/, [/moment$/]),
 
-    new ExtractTextPlugin('css/[name].css', {
+    new ExtractTextPlugin({
+      filename: 'css/[name].css',
       disable: false,
       allChunks: true
     }),
@@ -140,9 +168,8 @@ const config = {
 
   ],
   resolve: {
-    extensions: ['', '.js']
+    extensions: ['.js']
   }
 };
 
 module.exports = config;
-
